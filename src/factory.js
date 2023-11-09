6814890b769a2195b9aec3965a3af033e06dd377
gcattan <gcattan@hotmail.fr>
2023-11-09 22:00:11 +0100

    Update factory.js
    add Route helper

--- src/factory.js
+++ src/factory.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Router as ReactRouter, useLocation, useNavigate, useParams } from 'react-router';
+import { Router as ReactRouter, Route as ReactRoute, useLocation, useNavigate, useParams } from 'react-router';
 
 Router.propTypes = {
   history
@@ -32,4 +32,25 @@ export function withRouter(Component) {
   }
 
   return ComponentWithRouterProp;
+}
+
+Route.propTypes = {
+  exact,
+  component,
+  path
+};
+
+export function Route(props) {
+  const { exact, component, path } = props;
+  const renderSingleRoute = (single_path) => (
+    <ReactRoute
+      element={component}
+      path={`${single_path}{exact ? '' : '/*'}`}
+      {...props}
+    />
+  )
+  if(typeof(path) === 'string') {
+    return renderSingleRoute(path);
+  }
+  return path.map(single_path => renderSingleRoute(path)};
 }
