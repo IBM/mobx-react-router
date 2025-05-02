645c0e1feb8ac28731a2a6cb932ea7cb0af65be9
gcattan <gcattan@hotmail.fr>
2025-05-02 22:20:00 +0200

    Update types.spec.js

--- __test__/types.spec.js
+++ __test__/types.spec.js
@@ -19,7 +19,7 @@ describe('types', () => {
       noEmit: true,
       jsx: ts.JsxEmit.React,
       target: ts.ScriptTarget.ESNext,
-      moduleResolution: ts.ModuleResolutionKind.NodeNext
+      moduleResolution: ts.ModuleResolutionKind.Node10
     };
     const program = ts.createProgram(files, options);
     const diagnosticErrors = ts.getPreEmitDiagnostics(program)
