ce1ab3ce866904820c39af38ddaa13a7a10da660
gcattan <gcattan@hotmail.fr>
2025-05-02 22:02:35 +0200

    Update types.spec.js

--- __test__/types.spec.js
+++ __test__/types.spec.js
@@ -15,7 +15,11 @@ beforeEach(() => {
 describe('types', () => {
   it('can diagnose types without errors', () => {
     const files = [path.join(__dirname, '/types-test')];
-    const options = { noEmit: true, jsx: ts.JsxEmit.React };
+    const options = {
+      noEmit: true,
+      jsx: ts.JsxEmit.React,
+      target: "es2017"
+    };
     const program = ts.createProgram(files, options);
     const diagnosticErrors = ts.getPreEmitDiagnostics(program)
       .filter((diagnostic) => diagnostic.category === ts.DiagnosticCategory.Error)
